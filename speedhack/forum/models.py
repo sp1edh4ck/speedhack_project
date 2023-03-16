74ea72fe7df8f09b84f2bb37d07c0ca2099fddf3
Zakhar <94287281+sp11dh4ck@users.noreply.github.com>
2023-03-16 22:58:19 +0300

    #up

--- speedhack/forum/models.py
+++ speedhack/forum/models.py
@@ -1,4 +1,3 @@
-from django import forms
 from django.contrib.auth import get_user_model
 from django.db import models
 
@@ -6,104 +5,104 @@ User = get_user_model()
 
 
 class Ip(models.Model):
-    ip = models.CharField(max_length=100)
+	ip = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.ip
+	def __str__(self):
+		return self.ip
 
 
 class Group(models.Model):
-    title = models.CharField(unique=True, max_length=50, verbose_name='Название')
-    slug = models.SlugField(unique=True, verbose_name='Уникальный адрес')
+	title = models.CharField(unique=True, max_length=50, verbose_name='Название')
+	slug = models.SlugField(unique=True, verbose_name='Уникальный адрес')
 
-    class Meta:
-        verbose_name = 'Ранг'
-        verbose_name_plural = 'Ранги'
+	class Meta:
+		verbose_name = 'Ранг'
+		verbose_name_plural = 'Ранги'
 
-    def __str__(self):
-        return self.title
+	def __str__(self):
+		return self.title
 
 
 class Forum(models.Model):
-    title = models.CharField(verbose_name='Заголовок', max_length=55)
-    text = models.TextField(verbose_name='Текст', max_length=10000)
-    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата')
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        verbose_name='Автор',
-        related_name='posts'
-    )
-    image = models.ImageField(
-        'Картинка',
-        upload_to='posts/',
-        blank=True
-    )
-    views = models.ManyToManyField(Ip, related_name='views', verbose_name='Просмотры', default=0)
-    
-    def total_views(self):
-        return self.views.count()
-    
-    class Meta:
-        verbose_name = 'Пост'
-        verbose_name_plural = 'Посты'
-        ordering = ('-pub_date',)
+	title = models.CharField(verbose_name='Заголовок', max_length=55)
+	text = models.TextField(verbose_name='Текст', max_length=10000)
+	pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата')
+	author = models.ForeignKey(
+		User,
+		on_delete=models.CASCADE,
+		verbose_name='Автор',
+		related_name='posts'
+	)
+	image = models.ImageField(
+		'Картинка',
+		upload_to='posts/',
+		blank=True
+	)
+	views = models.ManyToManyField(Ip, related_name='views', verbose_name='Просмотры', default=0)
+	
+	def total_views(self):
+		return self.views.count()
+	
+	class Meta:
+		verbose_name = 'Пост'
+		verbose_name_plural = 'Посты'
+		ordering = ('-pub_date',)
 
-    def __str__(self):
-        number_of_chars = 15
-        return self.text[:number_of_chars]
+	def __str__(self):
+		number_of_chars = 15
+		return self.text[:number_of_chars]
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Forum,
-        on_delete=models.CASCADE,
-        related_name='comments',
-        verbose_name='Пост',
-    )
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='comments',
-        verbose_name='Автор',
-    )
-    text = models.TextField(
-        verbose_name='Текст комментария',
-        max_length=2000,
-    )
-    image = models.ImageField(
-        'Картинка',
-        upload_to='posts/images/',
-        blank=True
-    )
-    created = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='Дата публикации',
-    )
+	post = models.ForeignKey(
+		Forum,
+		on_delete=models.CASCADE,
+		related_name='comments',
+		verbose_name='Пост',
+	)
+	author = models.ForeignKey(
+		User,
+		on_delete=models.CASCADE,
+		related_name='comments',
+		verbose_name='Автор',
+	)
+	text = models.TextField(
+		verbose_name='Текст комментария',
+		max_length=2000,
+	)
+	image = models.ImageField(
+		'Картинка',
+		upload_to='posts/images/',
+		blank=True
+	)
+	created = models.DateTimeField(
+		auto_now_add=True,
+		verbose_name='Дата публикации',
+	)
 
-    class Meta:
-        verbose_name = 'Комментарий'
-        verbose_name_plural = 'Комментарии'
+	class Meta:
+		verbose_name = 'Комментарий'
+		verbose_name_plural = 'Комментарии'
 
 
 class Follow(models.Model):
-		user = models.ForeignKey(
-				User,
-				on_delete=models.CASCADE,
-				related_name='follower',
-		)
-		author = models.ForeignKey(
-				User,
-				on_delete=models.CASCADE,
-				related_name='following',
-		)
-
-		class Meta:
-				verbose_name = 'Подписка'
-				verbose_name_plural = 'Подписки'
-				constraints = [
-						models.UniqueConstraint(
-								fields=('user', 'author'),
-								name='unique_name_in_subsciber',
-						)
-				]
+	user = models.ForeignKey(
+		User,
+		on_delete=models.CASCADE,
+		related_name='follower',
+	)
+	author = models.ForeignKey(
+		User,
+		on_delete=models.CASCADE,
+		related_name='following',
+	)
+ 
+	class Meta:
+		verbose_name = 'Подписка'
+		verbose_name_plural = 'Подписки'
+		constraints = [
+			models.UniqueConstraint(
+				fields=('user', 'author'),
+				name='unique_name_in_subsciber',
+			)
+		]
--- speedhack/forum/views.py
+++ speedhack/forum/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views.decorators.cache import cache_page
 
 from users.models import CustomUser
 
@@ -9,168 +8,172 @@ from .forms import CommentForm, PostForm
 from .models import Follow, Forum, Ip, User
 
 
-def pagination(request, post_list):
-    paginator = Paginator(post_list, 12)
-    return paginator.get_page(request.GET.get('page'))
+def pagination_post(request, post_list):
+	paginator = Paginator(post_list, 12)
+	return paginator.get_page(request.GET.get('page'))
+
+
+def pagination_sub(request, sub_list):
+	paginator = Paginator(sub_list, 10)
+	return paginator.get_page(request.GET.get('page'))
 
 
 def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
+	x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+	if x_forwarded_for:
+		ip = x_forwarded_for.split(',')[0]
+	else:
+		ip = request.META.get('REMOTE_ADDR')
+	return ip
 
 
 def post_view(request, slug):
-    post = Forum.objects.get(slug=slug)
-
-    ip = get_client_ip(request)
-
-    if Ip.objects.filter(ip=ip).exists():
-        post.views.add(Ip.objects.get(ip=ip))
-    else:
-        Ip.objects.create(ip=ip)
-        post.views.add(Ip.objects.get(ip=ip))  
-    
-    context = {
-        'post' : post,
-    }
-    return render(request, 'main/post.html', context)
+	post = Forum.objects.get(slug=slug)
+	ip = get_client_ip(request)
+	if Ip.objects.filter(ip=ip).exists():
+		post.views.add(Ip.objects.get(ip=ip))
+	else:
+		Ip.objects.create(ip=ip)
+		post.views.add(Ip.objects.get(ip=ip))
+	context = {
+		'post' : post,
+	}
+	return render(request, 'main/post.html', context)
 
 
-# @cache_page(20, key_prefix='index_page')
 def index(request):
-    posts = Forum.objects.select_related('author').all()
-    template = 'forum/index.html'
-    context = {
-        'objects': pagination(request, posts)
-    }
-    return render(request, template, context)
+	posts = Forum.objects.select_related('author').all()
+	template = 'forum/index.html'
+	context = {
+		'objects': pagination_post(request, posts)
+	}
+	return render(request, template, context)
 
 
 def profile(request, username):
-    author = get_object_or_404(User, username=username)
-    group = ...
-    posts = author.posts.all()
-    following = (
-				author.following.filter(user_id=request.user.id).exists()
-				if request.user.is_authenticated
-				else False
+	author = get_object_or_404(User, username=username)
+	posts = author.posts.all()
+	subs = author.follower.all()
+	following = (
+		author.following.filter(user_id=request.user.id).exists()
+		if request.user.is_authenticated
+		else False
 		)
-    count_posts = posts.count()
-    template = 'forum/profile.html'
-    context = {
-        'author': author,
-        'following': following,
-        'count_posts': count_posts,
-        'objects': pagination(request, posts),
-    }
-    return render(request, template, context)
+	count_posts = posts.count()
+	count_subs = subs.count()
+	template = 'forum/profile.html'
+	context = {
+		'author': author,
+		'following': following,
+		'count_posts': count_posts,
+		'count_subs': count_subs,
+		'objects': pagination_post(request, posts),
+		'subs': pagination_sub(request, subs),
+	}
+	return render(request, template, context)
 
 
 def post_detail(request, post_id):
-    post = get_object_or_404(Forum, id=post_id)
-    form = CommentForm(request.POST or None)
-    comments = post.comments.all()
-    ip = get_client_ip(request)
-    if Ip.objects.filter(ip=ip).exists():
-        post.views.add(Ip.objects.get(ip=ip))
-    else:
-        Ip.objects.create(ip=ip)
-        post.views.add(Ip.objects.get(ip=ip))
-    template = 'forum/post_detail.html'
-    context = {
-				'form': form,
-        'post': post,
-        'comments': comments
-    }
-    return render(request, template, context)
+	post = get_object_or_404(Forum, id=post_id)
+	form = CommentForm(request.POST or None)
+	comments = post.comments.all()
+	ip = get_client_ip(request)
+	if Ip.objects.filter(ip=ip).exists():
+		post.views.add(Ip.objects.get(ip=ip))
+	else:
+		Ip.objects.create(ip=ip)
+		post.views.add(Ip.objects.get(ip=ip))
+	template = 'forum/post_detail.html'
+	context = {
+		'form': form,
+		'post': post,
+		'comments': comments
+	}
+	return render(request, template, context)
 
 
 @login_required
 def post_create(request):
-    form = PostForm(
-        request.POST or None,
-        files=request.FILES or None
-    )
-    if request.method == 'POST':
-        if form.is_valid():
-            new_post = form.save(commit=False)
-            new_post.author = request.user
-            new_post.save()
-            return redirect('forum:index')
-    template = 'forum/post_create.html'
-    context = {
-        'form': form
-    }
-    return render(request, template, context)
+	form = PostForm(
+		request.POST or None,
+		files=request.FILES or None
+	)
+	if request.method == 'POST':
+		if form.is_valid():
+			new_post = form.save(commit=False)
+			new_post.author = request.user
+			new_post.save()
+			return redirect('forum:index')
+	template = 'forum/post_create.html'
+	context = {
+		'form': form
+	}
+	return render(request, template, context)
 
 
 @login_required
 def add_comment(request, post_id):
-    post = get_object_or_404(Forum, pk=post_id)
-    form = CommentForm(request.POST or None)
-    if form.is_valid():
-        comment = form.save(commit=False)
-        comment.author = request.user
-        comment.post = post
-        comment.save()
-    return redirect('forum:post_detail', post_id=post_id)
+	post = get_object_or_404(Forum, pk=post_id)
+	form = CommentForm(request.POST or None)
+	if form.is_valid():
+		comment = form.save(commit=False)
+		comment.author = request.user
+		comment.post = post
+		comment.save()
+	return redirect('forum:post_detail', post_id=post_id)
 
 
 """
 @login_required
 def follow_index(request):
-		template = 'forum/follow.html'
- 		post_list = Forum.objects.filter(author__following__user=request.user).all()
- 		context = {
- 				'objects': pagination(request, post_list)
- 		}
- 		return render(request, template, context)
+	template = 'forum/follow.html'
+ 	post_list = Forum.objects.filter(author__following__user=request.user).all()
+ 	context = {
+ 		'objects': pagination(request, post_list)
+ 	}
+ 	return render(request, template, context)
 """
 
 
 @login_required
 def profile_follow(request, username):
-		author = get_object_or_404(User, username=username)
-		if author != request.user:
-				if not Follow.objects.filter(author=author, user=request.user).exists():
-						Follow.objects.create(author=author, user=request.user)
-		return redirect('forum:profile', username=username)
+	author = get_object_or_404(User, username=username)
+	if author != request.user:
+		if not Follow.objects.filter(author=author, user=request.user).exists():
+			Follow.objects.create(author=author, user=request.user)
+	return redirect('forum:profile', username=username)
 
 
 @login_required
 def profile_unfollow(request, username):
-		author = get_object_or_404(User, username=username)
-		author.following.filter(user=request.user).delete()
-		return redirect('forum:profile', username=username)
+	author = get_object_or_404(User, username=username)
+	author.following.filter(user=request.user).delete()
+	return redirect('forum:profile', username=username)
 
 
 @login_required
 def admin_panel(request):
-		template = 'forum/admin.html'
-		author = Forum.objects.select_related('author').all()
-		users_list = CustomUser.objects.all()
-		context = {
-				'author': author,
-				'users': users_list,
-		}
-		return render(request, template, context)
+	template = 'forum/admin.html'
+	author = Forum.objects.select_related('author').all()
+	users_list = CustomUser.objects.all()
+	context = {
+		'author': author,
+		'users': users_list,
+	}
+	return render(request, template, context)
 
 
 def faq(request):
-		template = 'forum/faq.html'
-		return render(request, template)
+	template = 'forum/faq.html'
+	return render(request, template)
 
 
 def users(request):
-		template = 'forum/users.html'
-		author = Forum.objects.select_related('author').all()
-		users_list = CustomUser.objects.all()
-		context = {
-				'author': author,
-				'users': users_list,
-		}
-		return render(request, template, context)
+	template = 'forum/users.html'
+	author = Forum.objects.select_related('author').all()
+	users_list = CustomUser.objects.all()
+	context = {
+		'author': author,
+		'users': users_list,
+	}
+	return render(request, template, context)
