114589a9c363d9993934c065615798be410fa576
Zakhar <94287281+sp11dh4ck@users.noreply.github.com>
2023-03-17 12:07:57 +0300

    #up

--- speedhack/static/js/profile.js
+++ speedhack/static/js/profile.js
@@ -21,3 +21,27 @@ tabs.forEach(function(item) {
 		}
 	});
 });
+
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+	modal.style.display = "block";
+}
+
+span.onclick = function() {
+	modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
+
+
+$(".arrow-4").click(function() {
+	$(this).toggleClass("open");
+});
